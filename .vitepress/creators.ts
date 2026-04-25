6c6c24934ac25f6c217986a0fb83e0815a44225f
SUPERMURLOC019 <156361186+SUPERMURLOC019@users.noreply.github.com>
2026-04-25 13:37:34 +0800

    Update creators.ts

--- .vitepress/creators.ts
+++ .vitepress/creators.ts
@@ -19,7 +19,7 @@ export interface Creator {
 export const creators: Creator[] = [
   {
     name: '只梨',
-    avatar: '/userdata/pic.png',
+    avatar: 'https://avatify.online/api/svg/eyJmYWNlIjo0LCJub3NlIjowLCJtb3V0aCI6MSwiZXllcyI6MTAsImV5ZWJyb3dzIjo1LCJnbGFzc2VzIjoyLCJoYWlyIjo1LCJhY2Nlc3NvcmllcyI6MCwiZGV0YWlscyI6MCwiYmVhcmQiOjAsImZsaXAiOjAsImNvbG9yIjoicmdiYSgyNTUsIDAsIDAsIDApIiwic2hhcGUiOiJub25lIn0=',
     username: 'Peerk',
     title: '主页作者',
     desc: '一个平平无奇的普通人',
